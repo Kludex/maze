14f9f2cbb1e3bdd276565cc96f6bd6c4c730372e
Marcelo Trylesinski <marcelotryle@gmail.com>
2020-02-02 11:33:44 -0300

    Add newer versions of each class

---
+++ maze_generator2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from maze2 import Maze
+
+class MazeGenerator():
+
+    @property
+    def maze(self):
+        return self._maze
+
+    def __init__(self, length=40, width=40):
+        self._maze = Maze(length, width)
+        self.randomize()
+
+    def randomize(self):
+        visited = set()
+
+        for row in range(len(self.maze.maze)):
+            for col in range(len(self.maze.maze[row])):
+                self.dfs((row, col), visited)
+
+    # Remember to randomize chosen walls
+    def dfs(self, position: tuple, visited: set):
+        visited.add(position)
+        walls = self.maze.maze[position[0]][position[1]].walls.copy()
+
+        for wall in walls:
+            if self.maze.is_valid(wall) and wall not in visited:
+                self.break_wall(position, wall)
+                self.dfs(tuple(wall), visited)
+
+    def break_wall(self, cell1: tuple, cell2: tuple):
+        self.maze.maze[cell1[0]][cell1[1]].break_wall(cell2)
+        self.maze.maze[cell2[0]][cell2[1]].break_wall(cell1)
+
+    def display(self):
+        self.maze.display()
---
+++ maze_solver2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from maze2 import Maze
+
+class MazeSolver():
+
+    @property
+    def maze(self):
+        return self._maze
+
+    @property
+    def path(self):
+        return self._path
+
+    def __init__(self, maze: Maze, entrance: tuple, exit: tuple):
+        self._maze = maze
+        self._path = self.solve(entrance, exit)
+        print(self.path)
+
+    def solve(self, entrance: tuple, exit: tuple) -> list:
+        visited = set()
+        return self.shortest_path(entrance, exit, visited)
+
+    def shortest_path(self, entrance: tuple, exit: tuple, visited: set) -> list:
+        queue = [entrance]
+        level = [[-1] * len(self.maze.maze[row]) for row in range(len(self.maze.maze))]
+        level[entrance[0]][entrance[1]] = 0
+        visited.add(entrance)
+
+        while queue and ((cell := queue[0]) != exit):
+            queue.pop(0)
+            for neighbor in self.maze.available_paths(cell):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    level[neighbor[0]][neighbor[1]] = level[cell[0]][cell[1]] + 1
+                    queue.append(neighbor)
+
+        cell = exit
+        path = []
+        while level[cell[0]][cell[1]] > 0:
+            path.append(cell)
+            for neighbor in self.maze.available_paths(cell):
+                if level[neighbor[0]][neighbor[1]] + 1 == level[cell[0]][cell[1]]:
+                    cell = neighbor
+        path.append(cell)
+        return list(reversed(path))
+
+    def display(self):
+        self.maze.display(self.path)
